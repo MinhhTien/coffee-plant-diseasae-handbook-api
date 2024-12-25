e0e83895d414e9ab091527aa7d050cab0e873db4
Martin Vo <92145479+MinhhTien@users.noreply.github.com>
2024-12-25 10:22:19 +0700

    feat: disease

--- src/disease/services/disease.service.ts
+++ src/disease/services/disease.service.ts
@@ -32,78 +32,78 @@ export class DiseaseService implements IDiseaseService {
     private readonly diseaseRepository: IDiseaseRepository
   ) {}
 
-  async onModuleInit() {
-    await this.diseaseRepository.model.insertMany([
-      {
-        name: 'Bệnh tảo đỏ',
-        reason: 'Vi khuẩn Pseudomonas và nấm Colletotrichum',
-        description:
-          'Bệnh táo đỏ cà phê là bệnh do vi khuẩn và nấm gây ra, đặc trưng của bệnh là xuất hiện các vết đỏ, loang màu và tụ điểm màu đỏ trên lá và quả của cây cà phê. Đây là một bệnh hại rất nguy hiểm gây suy yếu cây cà phê, ảnh hưởng đến chất lượng và năng suất của cây.',
-        effectedVarieties: [
-          new Types.ObjectId('676aca6f94501af32c8a3c6e'),
-          new Types.ObjectId('676aca6f94501af32c8a3c6f'),
-          new Types.ObjectId('676aca6f94501af32c8a3c70')
-        ],
-        symptoms: [
-          {
-            name: 'Chùm, lá, quả trở nên khô héo',
-            level: SymptomLevel.LOW
-          },
-          {
-            name: 'Lá và quả bao phủ một lớp phấn hồng bám chặt',
-            level: SymptomLevel.MEDIUM
-          },
-          {
-            name: 'Tổn thương trên bề mặt lá',
-            level: SymptomLevel.HIGH
-          },
-          {
-            name: 'Lá cây có thể bị rụng sớm',
-            level: SymptomLevel.HIGH
-          }
-        ],
-        preventions: [
-          {
-            name: 'Sử dụng thuốc bảo vệ thực vật',
-            type: PreventionType.CHEMISTRY,
-            effective: 5,
-            instruction:
-              'Lựa chọn thuốc phù hợp: Tùy theo loại vi khuẩn hoặc nấm gây bệnh, chọn các loại thuốc trừ bệnh có tác động hiệu quả và không gây hại cho môi trường và con người.',
-            note: 'Tuân thủ hướng dẫn sử dụng và liều lượng được đề xuất trên nhãn sản phẩm để tránh tác động phụ không mong muốn và đảm bảo hiệu quả khắc phục bệnh.'
-          },
-          {
-            name: 'Sử dụng biện pháp sinh học và công nghệ',
-            type: PreventionType.BIOLOGY,
-            effective: 5,
-            instruction: 'Sử dụng máy đo nông đọ đất ẩm.',
-            note: ''
-          },
-          {
-            name: 'Cắt và bảo vệ cây xanh',
-            type: PreventionType.PHYSICS,
-            effective: 3,
-            instruction: 'Cắt tỉa các cành bệnh , giữ lại các cạnh còn xanh tốt.',
-            note: ''
-          },
-          {
-            name: 'Giám sát và kiểm tra thường xuyên',
-            type: PreventionType.PHYSICS,
-            effective: 3,
-            instruction:
-              'Kiểm tra thường xuyên đặc biệt là vào mùa mưa khi môi trường ẩm ướt dễ dàng cho nấm sinh trưởng.',
-            note: ''
-          }
-        ],
-        time: {
-          startTime: new Date('2023-04-01'),
-          endTime: new Date('2023-06-31'),
-          season: SeasonType.SPRING
-        },
-        image:
-          'https://res.cloudinary.com/dbimwvmcy/image/upload/v1735096178/coffee-plant-handbook/uctapjpj63wtbc852n2n.png'
-      }
-    ])
-  }
+  // async onModuleInit() {
+  //   await this.diseaseRepository.model.insertMany([
+  //     {
+  //       name: 'Bệnh tảo đỏ',
+  //       reason: 'Vi khuẩn Pseudomonas và nấm Colletotrichum',
+  //       description:
+  //         'Bệnh táo đỏ cà phê là bệnh do vi khuẩn và nấm gây ra, đặc trưng của bệnh là xuất hiện các vết đỏ, loang màu và tụ điểm màu đỏ trên lá và quả của cây cà phê. Đây là một bệnh hại rất nguy hiểm gây suy yếu cây cà phê, ảnh hưởng đến chất lượng và năng suất của cây.',
+  //       effectedVarieties: [
+  //         new Types.ObjectId('676aca6f94501af32c8a3c6e'),
+  //         new Types.ObjectId('676aca6f94501af32c8a3c6f'),
+  //         new Types.ObjectId('676aca6f94501af32c8a3c70')
+  //       ],
+  //       symptoms: [
+  //         {
+  //           name: 'Chùm, lá, quả trở nên khô héo',
+  //           level: SymptomLevel.LOW
+  //         },
+  //         {
+  //           name: 'Lá và quả bao phủ một lớp phấn hồng bám chặt',
+  //           level: SymptomLevel.MEDIUM
+  //         },
+  //         {
+  //           name: 'Tổn thương trên bề mặt lá',
+  //           level: SymptomLevel.HIGH
+  //         },
+  //         {
+  //           name: 'Lá cây có thể bị rụng sớm',
+  //           level: SymptomLevel.HIGH
+  //         }
+  //       ],
+  //       preventions: [
+  //         {
+  //           name: 'Sử dụng thuốc bảo vệ thực vật',
+  //           type: PreventionType.CHEMISTRY,
+  //           effective: 5,
+  //           instruction:
+  //             'Lựa chọn thuốc phù hợp: Tùy theo loại vi khuẩn hoặc nấm gây bệnh, chọn các loại thuốc trừ bệnh có tác động hiệu quả và không gây hại cho môi trường và con người.',
+  //           note: 'Tuân thủ hướng dẫn sử dụng và liều lượng được đề xuất trên nhãn sản phẩm để tránh tác động phụ không mong muốn và đảm bảo hiệu quả khắc phục bệnh.'
+  //         },
+  //         {
+  //           name: 'Sử dụng biện pháp sinh học và công nghệ',
+  //           type: PreventionType.BIOLOGY,
+  //           effective: 5,
+  //           instruction: 'Sử dụng máy đo nông đọ đất ẩm.',
+  //           note: ''
+  //         },
+  //         {
+  //           name: 'Cắt và bảo vệ cây xanh',
+  //           type: PreventionType.PHYSICS,
+  //           effective: 3,
+  //           instruction: 'Cắt tỉa các cành bệnh , giữ lại các cạnh còn xanh tốt.',
+  //           note: ''
+  //         },
+  //         {
+  //           name: 'Giám sát và kiểm tra thường xuyên',
+  //           type: PreventionType.PHYSICS,
+  //           effective: 3,
+  //           instruction:
+  //             'Kiểm tra thường xuyên đặc biệt là vào mùa mưa khi môi trường ẩm ướt dễ dàng cho nấm sinh trưởng.',
+  //           note: ''
+  //         }
+  //       ],
+  //       time: {
+  //         startTime: new Date('2023-04-01'),
+  //         endTime: new Date('2023-06-31'),
+  //         season: SeasonType.SPRING
+  //       },
+  //       image:
+  //         'https://res.cloudinary.com/dbimwvmcy/image/upload/v1735096178/coffee-plant-handbook/uctapjpj63wtbc852n2n.png'
+  //     }
+  //   ])
+  // }
 
   public create(Disease: any, options?: SaveOptions | undefined) {
     return this.diseaseRepository.create(Disease, options)
